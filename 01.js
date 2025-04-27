eabe6b97bacc4eb397d0bbde30772a5a6bb0c20b
nikunjwandile1234 <nikunjwandile123@gmail.com>
2025-04-27 14:16:45 +0530

    01

---
+++ 01.js
@@ -0,0 +1,40 @@
+let score="33abc"
+console.log(typeof(score));
+let valueinno=Number(score)
+console.log(typeof(score));
+console.log(typeof(valueinno));
+
+
+
+let username ="digvijay";
+let password="dighu1";
+
+console.log(`the username is ${username}, the password is ${password}`);
+let gamecounter =100;
+gamecounter++;
+console.log(gamecounter);
+
+console.log(`the gamecounter is ${gamecounter}`);
+
+let str1='hello'
+let str2='nikunj'
+
+let a=str1+" "+str2;
+console.log(`The string concat is ${a}`);
+console.log("1"+2);
+console.log(1+2+"2");
+
+
+let name=undefined;
+console.log(typeof(name));
+
+let sname=null;
+console.log(typeof(sname));
+
+
+
+
+
+
+
+
