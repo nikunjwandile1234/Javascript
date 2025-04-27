3f0ce6aa74ded869121b53d50f30ea1f883cbd7f
nikunjwandile1234 <nikunjwandile123@gmail.com>
2025-04-27 14:38:54 +0530

    02

---
+++ 02.js
@@ -0,0 +1,29 @@
+let getname= new String("Nikunj Wandile");
+console.log(getname);
+
+console.log(getname.length);
+console.log(getname.charAt(4));
+console.log(getname.indexOf(getname,4));
+console.log(getname.substring(0,4));
+const anotherstring =getname.slice(-8,4);
+console.log(anotherstring);
+
+
+const newstringone ="         nikunj wandile       ";
+console.log(newstringone);
+console.log(newstringone.trim());
+
+
+const url="https//hitesh.com/hitesh%20choudhary";
+console.log(url.replace("%20","-"));//works only in copy of manner 
+console.log(url);
+
+
+
+
+
+
+
+
+
+
